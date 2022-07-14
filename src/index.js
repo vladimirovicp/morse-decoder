c7e168a0fbcd2cb99d948aad2090b032dc078ea0
vladimirovicp <vladimirovicp70@gmail.com>
2022-07-14 10:31:59 +0400

    feat: final

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let code = [];
+    const simvolToCode = {
+        '10': '.',
+        '11': '-',
+        '**': ' '
+    };
+
+    let codeOne = '';
+    let codeNormal = '';
+    let myIndex = 0;
+
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        codeOne = expr.substr(i, 10);
+        codeOne[0] === '*' ? codeNormal = '**' : codeNormal = codeOne.slice(codeOne.indexOf('1'));
+
+        if (codeNormal.length === 2) {
+            code[myIndex] = simvolToCode[codeNormal];
+        } else {
+            let layout = '';
+
+            for (let j = 0; j < codeNormal.length; j = j + 2) {
+                layout = layout + simvolToCode[codeNormal[j] + codeNormal[j + 1]];
+            }
+            code[myIndex] = layout;
+        }
+        myIndex++;
+    }
+
+    let text = code.map((value, index) => {
+        if (code[index] === ' ') {
+            return ' ';
+        } else {
+            return MORSE_TABLE[code[index]];
+        }
+    });
+
+
+
+    return text.join('');
 }
 
 module.exports = {
