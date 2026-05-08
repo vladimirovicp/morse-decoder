8ff184cff2b0ddcd500bbdb0cf85f1c4d1a1de5c
vladimirovicp <vladimirovicp70@gmail.com>
2026-05-08 11:06:44 +0400

    feat: Standardize task format

--- src/index.js
+++ src/index.js
@@ -1,85 +1,78 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
-function decode(expr) {
-    let code = [];
-    const simvolToCode = {
-        '10': '.',
-        '11': '-',
-        '**': ' '
-    };
+module.exports = function decode(expr) {
+  const code = [];
+  const simvolToCode = {
+    10: '.',
+    11: '-',
+    '**': ' ',
+  };
 
-    let codeOne = '';
-    let codeNormal = '';
-    let myIndex = 0;
+  let codeOne = '';
+  let codeNormal = '';
+  let myIndex = 0;
 
+  for (let i = 0; i < expr.length; i += 10) {
+    codeOne = expr.substr(i, 10);
+    codeNormal =
+      codeOne[0] === '*' ? '**' : codeOne.slice(codeOne.indexOf('1'));
 
-    for (let i = 0; i < expr.length; i = i + 10) {
-        codeOne = expr.substr(i, 10);
-        codeOne[0] === '*' ? codeNormal = '**' : codeNormal = codeOne.slice(codeOne.indexOf('1'));
+    if (codeNormal.length === 2) {
+      code[myIndex] = simvolToCode[codeNormal];
+    } else {
+      let layout = '';
 
-        if (codeNormal.length === 2) {
-            code[myIndex] = simvolToCode[codeNormal];
-        } else {
-            let layout = '';
-
-            for (let j = 0; j < codeNormal.length; j = j + 2) {
-                layout = layout + simvolToCode[codeNormal[j] + codeNormal[j + 1]];
-            }
-            code[myIndex] = layout;
-        }
-        myIndex++;
+      for (let j = 0; j < codeNormal.length; j += 2) {
+        layout += simvolToCode[codeNormal[j] + codeNormal[j + 1]];
+      }
+      code[myIndex] = layout;
     }
+    myIndex += 1;
+  }
 
-    let text = code.map((value, index) => {
-        if (code[index] === ' ') {
-            return ' ';
-        } else {
-            return MORSE_TABLE[code[index]];
-        }
-    });
+  const text = code.map((value, index) => {
+    if (code[index] === ' ') {
+      return ' ';
+    }
+    return MORSE_TABLE[code[index]];
+  });
 
-
-
-    return text.join('');
-}
-
-module.exports = {
-    decode
-}
+  return text.join('');
+};
